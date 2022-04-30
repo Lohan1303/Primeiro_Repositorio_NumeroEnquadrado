6547e2525b810eae7e823380721889bf77c98935
Lohan1303 <lohanbatista2003@gmail.com>
2022-04-30 16:05:12 -0300

    Número enquadrado
    Esse algoritmo permite achar 'números enquadrados' entre uma sequência de números.

---
+++ Numero Enquadrado/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Números enquadrados
+     
+ 
+ 
+ */
+
+static int Inverter(int numero)
+{
+
+    string valor = numero.ToString();
+    string resultado = "";
+    for (int i = valor.Length; i > 0; i--)
+    {
+        resultado += valor[i - 1];
+    }
+
+    return int.Parse(resultado);
+}
+
+for (int i = 10; i < 99; i++)
+{
+    int resultado = i + Inverter(i);
+    int raiz = Convert.ToInt16(Math.Sqrt(resultado));
+    if (Math.Sqrt(resultado) == raiz)
+        Console.WriteLine(i);
+}
